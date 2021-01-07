0b0ed8f90306269a8dc45917e768cb6ac38de8e5
Kaden Biel <kaden.secondday@gmail.com>
2021-01-06 21:18:57 -0500

    App opens on settings page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 	{
 		path: '',
-		redirectTo: 'main',
+		redirectTo: 'settings',
 		pathMatch: 'full',
 	},
 	{
